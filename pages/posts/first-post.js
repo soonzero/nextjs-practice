7b84352f2ed5a2699d42ee641333bb05e6b58d46
soonzero <sykeum0912@naver.com>
2022-05-31 01:34:15 +0900

    [2022.05.30]

---
+++ pages/posts/first-post.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Image
+        src="/images/profile.png"
+        height={144}
+        width={144}
+        alt="Profile Image"
+      />
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+}
